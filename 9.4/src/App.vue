7fc4a4ded3dc124c9944d4cc2b9c41b17c907a3a
Wang-Hui-Liu <2422967114@qq.com>
2019-09-04 09:15:22 +0800

    9.4

---
+++ 9.4/src/App.vue
@@ -0,0 +1,27 @@
+<template>
+  <div id="app">
+    <ul>
+      <!-- 遍历数据 -->
+      <li v-for="(item,index) in array" :key="index">{{item}}</li>
+    </ul>
+  </div>
+</template>
+
+<script>
+export default {
+  data(){
+    return {
+      array:['a','2','4','5g','h']
+    }
+  }
+}
+</script>
+
+<style lang="scss">
+li{
+  height: 40px;
+  line-height: 40px;
+  list-style: none;
+  border-bottom: 1px solid #ccc;
+}
+</style>
